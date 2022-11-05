051d4a7ccdefe560a83aba7995e28952954a61f0
Felix Stupp <felix.stupp@banananet.work>
2022-11-05 14:14:47 +0100

    Added logging to suitable extractor search

--- server/entertainment_decider/extractors/helpers.py
+++ server/entertainment_decider/extractors/helpers.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from typing import Iterable, Optional, TypeVar
 
 from .generic import ExtractionError, GeneralExtractor
@@ -13,9 +14,14 @@ def search_suitable_extractor(extractor_list: Iterable[T], uri: str) -> Optional
     for extractor in extractor_list:
         match = extractor.uri_suitable(uri)
         if match.accept_immediately:
+            logging.debug(f"Accept {extractor.name} immediataly for {uri!r}")
             return extractor
         if match.can_accept and best_bet is None:
             best_bet = extractor
+    if best_bet:
+        logging.debug(f"Accept {best_bet.name} as fallback for {uri!r}")
+    else:
+        logging.debug(f"None found for {uri!r}")
     return best_bet
 
 
