a3ea1548aa26a0c58276477a81052f30535bcb63
Felix Stupp <felix.stupp@banananet.work>
2023-04-14 15:52:38 +0200

    entertainment_decider/models/init: Add __all__

--- server/entertainment_decider/models/__init__.py
+++ server/entertainment_decider/models/__init__.py
@@ -32,3 +32,30 @@ from .predefined_tags import (
 from .thumbnails import (
     thumbnail_sort_key,
 )
+
+
+__all__ = [
+    "CollectionStats",
+    "CollectionUriMapping",
+    "MediaCollection",
+    "MediaCollectionLink",
+    "MediaElement",
+    "MediaThumbnail",
+    "MediaThumbnailCache",
+    "MediaUriMapping",
+    "Query",
+    "SafeStr",
+    "Tag",
+    "Tagable",
+    "TagKey",
+    "are_multiple_considered",
+    "db",
+    "get_all_considered",
+    "get_all_elements_tags_recursive",
+    "predefined_movie_tag",
+    "predefined_series_tag",
+    "predefined_video_tag",
+    "setup_custom_tables",
+    "thumbnail_sort_key",
+    "update_element_lookup_cache",
+]
