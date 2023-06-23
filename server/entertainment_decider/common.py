6cfc1fcf3ae6627ae7ffa9edb9e340d376fa9195
Felix Stupp <felix.stupp@banananet.work>
2023-06-23 22:39:11 +0200

    common: Add already used helpers fix_iter & iter_lookahead

--- server/entertainment_decider/common.py
+++ server/entertainment_decider/common.py
@@ -5,12 +5,15 @@ import sys
 from typing import (
     IO,
     Iterable,
+    Iterator,
     List,
     Literal,
     Optional,
     Sequence,
+    Tuple,
     TypeVar,
     Union,
+    overload,
 )
 
 
@@ -76,6 +79,76 @@ T = TypeVar("T")
 
 def limit_iter(iter: Iterable[T], limit: int) -> List[T]:
     return list(itertools.islice(iter, limit))
+
+
+class _IterFixer(Iterator[T]):
+    __it: Iterator[T]
+
+    def __init__(self, it: Iterator[T]) -> None:
+        super().__init__()
+        self.__it = it
+
+    def __iter__(self) -> Iterator[T]:
+        return self
+
+    def __next__(self) -> T:
+        return next(self.__it)
+
+
+def fix_iter(iterable: Iterable[T]) -> Iterable[T]:
+    return _IterFixer(iter(iterable))
+
+
+@overload
+def iter_lookahead(
+    iterable: Iterable[T],
+    get_first: Literal[False] = False,
+    get_last: Literal[False] = False,
+) -> Iterable[Tuple[T, T]]:
+    ...
+
+
+@overload
+def iter_lookahead(
+    iterable: Iterable[T],
+    get_first: Literal[True],
+    get_last: Literal[False] = False,
+) -> Iterable[Tuple[None, T] | Tuple[T, T]]:
+    ...
+
+
+@overload
+def iter_lookahead(
+    iterable: Iterable[T],
+    get_first: Literal[False] = False,
+    get_last: Literal[True] = True,  #  <- default only to satisfy python
+) -> Iterable[Tuple[T, T] | Tuple[T, None]]:
+    ...
+
+
+@overload
+def iter_lookahead(
+    iterable: Iterable[T],
+    get_first: Literal[True],
+    get_last: Literal[True],
+) -> Iterable[Tuple[None, T] | Tuple[T, T] | Tuple[T, None]]:
+    ...
+
+
+def iter_lookahead(
+    iterable: Iterable[T],
+    get_first: bool = False,
+    get_last: bool = False,
+) -> Iterable[Tuple[None, T] | Tuple[T, T] | Tuple[T, None]]:
+    it = iter(iterable)
+    last = next(it)
+    if get_first:
+        yield None, last
+    for cur in it:
+        yield last, cur
+        last = cur
+    if get_last:
+        yield last, None
 
 
 def date_to_datetime(d: date) -> datetime:
