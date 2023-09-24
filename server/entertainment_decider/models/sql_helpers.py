dc8fe056490ebd43920923f584bc1cae387cc26f
Felix Stupp <felix.stupp@banananet.work>
2023-09-24 23:18:41 +0200

    models/sql_helpers: add sql_condition_join

--- server/entertainment_decider/models/sql_helpers.py
+++ server/entertainment_decider/models/sql_helpers.py
@@ -13,5 +13,16 @@ def sql_cleanup(sql: str) -> str:
     return SQL_WHITESPACE_PATTERN.sub(" ", sql).strip()
 
 
+def sql_condition_join(
+    *sql_conditions: str | None,
+    joiner: str = "AND",
+    default: str = "TRUE",
+) -> str:
+    return (
+        f" {joiner} ".join(f"({cond})" for cond in sql_conditions if cond is not None)
+        or default
+    )
+
+
 def sql_where_in(id: str, id_list: Iterable[str | int]) -> str:
     return f"{id} IN ({','.join(str(i) for i in id_list)})"
